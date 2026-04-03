39e2575a94abc7a462a8118e5d70afacbfa0cc81
taynarad25 <taynarad25@gmail.com>
2026-04-03 15:34:27 +0000

    ajustando o gitignore

--- chatbot.js
+++ chatbot.js
@@ -83,6 +83,15 @@ const etapas = {};
 // =====================================
 const client = new Client({
   authStrategy: new LocalAuth(),
+  puppeteer:{
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-gpu'
+    ]
+  }
 });
 
 // =====================================
