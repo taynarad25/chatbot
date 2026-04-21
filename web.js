030ac6673973a2e301094c58fcc35511d7816662
taydms <taynarad25@gmail.com>
2026-04-21 15:24:43 -0300

    refactor: rename variable 'path' to 'pathName' for clarity in startWebServer function

--- web.js
+++ web.js
@@ -334,7 +334,7 @@ function renderIndexHtml() {
 function startWebServer({ getStatus, startClient, cancelQr, disconnectClient }) {
   const server = http.createServer(async (req, res) => {
     const url = new URL(req.url, `http://${req.headers.host || 'localhost'}`);
-    const path = url.pathname;
+    const pathName = url.pathname;
 
     if (req.method === 'GET' && pathName === '/login') {
       return sendHtml(res, renderLoginHtml());
